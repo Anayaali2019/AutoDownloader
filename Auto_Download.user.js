d3e11bc45a474381878b8f51e653b5c5c06cf5f5
PrideSoft <65437842+Anayaali2019@users.noreply.github.com>
2024-03-01 03:11:21 +0500

    Update Auto_Download.user.js

--- Auto_Download.user.js
+++ Auto_Download.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         DropGalaxy For MMSBEE
+// @name         easy downloader
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  DropGalaxy For MMSBEE
